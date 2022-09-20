77b90dcadb77cbb788332daf2867ac1037fbd7bc
Leonardo <leonardobadell@hotmail.com>
2022-09-20 19:19:11 -0300

    Already tested and add all files to feature

--- tests/cloneArray.test.js
+++ tests/cloneArray.test.js
@@ -2,5 +2,5 @@ const cloneArray = require('./../src/cloneArray')
 
 test('This will clone an array', () =>{
     const array = [1, 2, 3]
-expect(cloneArray(array)).toEqual(array)
+expect(cloneArray(array)).not.toBe(array)
 })
